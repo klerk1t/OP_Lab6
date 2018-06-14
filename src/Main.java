7c222dd7848e9618d7adf3a283a12087a628d52e
klerk1t <mr.bogdan123@gmail.com>
2018-06-14 12:48:18 +0300

    another commit

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,29 @@ public class Main {
 
         String inputFile = new String(data);
 
+        String operation[] = inputFile.split(";");
 
+        int count = 0;
 
-        System.out.println(inputFile);
+        for(int i = 0; i < operation.length; i++) {
+            if(!operation[i].substring(0, 1).equals("S")) {
+                count++;
+            }
+        }
+        String[] var = new String[count];
+        int hashTable[] = new int[count];
+
+        for(int i = 0; i < operation.length; i++) {
+            if(!operation[i].substring(0, 1).equals("S")) {
+                hashTable[i] = Integer.parseInt(operation[i].substring(operation[i].lastIndexOf(" ") + 1));
+                var[i] = operation[i].substring(0, operation[i].indexOf(" "));
+            }
+        }
+        for(String x : var)
+            System.out.println(x);
+
+        for(int x : hashTable)
+            System.out.println(x);
+        //System.out.println(inputFile);
     }
 }
