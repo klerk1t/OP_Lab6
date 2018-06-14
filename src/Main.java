0351ee6d2c5e47b4628f9cb8242d3a2ae0034f4a
klerk1t <mr.bogdan123@gmail.com>
2018-06-14 11:53:03 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws  IOException{
+        FileInputStream fileInputStream = new FileInputStream("res/input.txt");
+        int length = fileInputStream.available();
+        byte data[] = new byte[length];
+
+        fileInputStream.read(data);
+        fileInputStream.close();
+
+        String inputFile = new String(data);
+
+
+
+        System.out.println(inputFile);
     }
 }
