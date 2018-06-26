07261112091dcb1afa1ca886f623ef8eb8dd702d
klerk1t <mr.bogdan123@gmail.com>
2018-06-26 14:21:55 +0300

    and another one commit

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,32 @@
 public class Tree {
 
+    TreeNode root;
+
+    public void addTreeNode(int key, String name) {
+        TreeNode newTreeNode = new TreeNode(key, name);
+        if(root == null) {
+            root = newTreeNode;
+        } else {
+            TreeNode focusTreeNode = root;
+            TreeNode parent;
+            while (true){
+                parent = focusTreeNode;
+
+                if(key < focusTreeNode.key) {
+                    focusTreeNode = focusTreeNode.leftChild;
+                    if(focusTreeNode == null) {
+                        parent.leftChild = newTreeNode;
+                        return;
+                    }
+                } else {
+                    focusTreeNode = focusTreeNode.rightChild;
+                    if(focusTreeNode == null) {
+                        parent.rightChild = newTreeNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
 }
---
+++ src/TreeNode.java
@@ -0,0 +1,13 @@
+public class TreeNode {
+    int key;
+    String name;
+
+    TreeNode leftChild;
+    TreeNode rightChild;
+
+    TreeNode(int key, String name) {
+        this.key = key;
+        this.name = name;
+    }
+
+}
